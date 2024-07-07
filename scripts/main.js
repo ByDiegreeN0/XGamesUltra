a45ab213fa8f75de4ff885a23c87d92a6208b06f
Diego <diegoalejandrogarcia140902@gmail.com>
2024-07-07 15:53:35 -0500

    se agrego el proyecto al repositorio

---
+++ scripts/main.js
@@ -0,0 +1,19 @@
+window.sr = ScrollReveal();
+
+sr.reveal('.main-sec',{
+    duration: 1000,
+    origin: 'left',
+    distance: '-30px'
+});
+
+sr.reveal('.categories-container',{
+    duration: 1000,
+    origin: 'top',
+    distance: '-30px'
+});
+
+sr.reveal('.recomendations-container',{
+    duration: 1000,
+    origin: 'right',
+    distance: '-130px'
+});
