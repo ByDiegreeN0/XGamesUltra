2ada69c130e0b507b838ed03885cc6f7b450648a
Diego <diegoalejandrogarcia140902@gmail.com>
2024-07-11 18:22:31 -0500

    se cambio levemente el nav

--- scripts/main.js
+++ scripts/main.js
@@ -19,7 +19,17 @@ sr.reveal('.recomendations-container',{
 });
 
 
+document.addEventListener('DOMContentLoaded', function () {
+  const navLi = document.querySelector('.nav-li');
 
+  navLi.addEventListener('mouseover', function () {
+      this.querySelector('.submenu').style.display = 'block';
+  });
+
+  navLi.addEventListener('mouseout', function () {
+      this.querySelector('.submenu').style.display = 'none';
+  });
+}); // SUBMENU DE NAV
 
 
 let currentIndex = 0;
